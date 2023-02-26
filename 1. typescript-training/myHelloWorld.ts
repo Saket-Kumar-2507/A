ef45017759dd1b6c490f9935cd3d795a1c16b2bb
Saket Kumar <2018meb1260@iitrpr.ac.in>
2023-02-26 15:54:06 +0530

    Test code

---
+++ 1. typescript-training/myHelloWorld.ts
@@ -0,0 +1,7 @@
+// 1.) Make the file named "myHelloWorld.ts" and write the code in the file
+// 2.) In the terminal, run the command "tsc myHelloWorld.ts" -> this will tranpile the .ts file into .js file with the same name
+// 3.) Now you have JS file named "myHelloWorld.js" generated in your folder
+// 4.) Run the command "node myHelloWorld.js" in the terminal and the output will be generated
+
+// Search what is "Transpiling"
+console.log("Hello World");
